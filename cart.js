6c34fbedf63be61312552ba01260e0e7cebc44cd
plotva <huseva.a@gmail.com>
2024-04-05 23:56:29 +0200

    Add rendering cart products

---
+++ cart.js
@@ -0,0 +1,43 @@
+import { getElement } from './utils.js';
+
+const cartProductsContainer = getElement('.cart__inner');
+const totalPrice = getElement('.cart__total-price');
+
+let cart = JSON.parse(localStorage.getItem('cart'));
+if (!cart) {
+  cart = [];
+}
+
+const renderCartProducts = (products, containerDOM) => {
+  const renderedProducts = products.map((product) => {
+    const { id, title, image, price, count } = product;
+    return `<article class="cart-product">
+  <img src="${image}" alt="${image}" class="cart-product__img" />
+  <h3 class="cart-product__title">${title}</h3>
+  <p class="cart-product__price">${price} PLN</p>
+  <button class="btn cart-product__remove-btn" data-id="${id}">
+    remove
+  </button>
+  <button class="btn cart-product__decrease-btn">-</button>
+  <p class="cart-product__count">${count}</p>
+  <button class="btn cart-product__increase-btn">+</button>
+</article>`;
+  }).join``;
+
+  containerDOM.innerHTML = renderedProducts;
+};
+
+renderCartProducts(cart, cartProductsContainer);
+
+const setTotalValue = () => {
+  if (!cart) {
+    totalPrice.innerHTML = 0;
+  } else {
+    totalPrice.innerHTML =
+      cart.reduce(
+        (sum, product) => sum + product.price * 100 * product.count,
+        0
+      ) / 100;
+  }
+};
+setTotalValue();
