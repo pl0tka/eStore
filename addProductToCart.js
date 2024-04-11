ee212d393195138cd28bc22123858f26257f4cb7
plotva <huseva.a@gmail.com>
2024-04-11 12:00:20 +0200

    Refactor addToCart function
    Update and rename addSingleProductToCart to addToCart for general use

--- addProductToCart.js
+++ addProductToCart.js
@@ -1,23 +1,20 @@
-import { setStorageItem, getStorageItem } from './utils.js';
+import { getElement, setStorageItem, getStorageItem } from './utils.js';
 import { displayTotalProductCount } from './common.js';
 
-export const addSingleProductToCart = (
-  productId,
-  products,
-  totalProductCountDOM
-) => {
+const totalProductCount = getElement('.nav__cart-count');
+
+export const addToCart = (productId, selectedProduct, count = 1) => {
   let cart = getStorageItem('cart');
-  const selectedProduct = products.find((product) => product.id === productId);
   const productInCart = cart.find((product) => product.id === productId);
+  const productIndex = cart.findIndex((product) => product.id === productId);
 
   // set product to add
   if (!productInCart) {
-    cart.push({ ...selectedProduct, count: 1 });
+    cart.push({ ...selectedProduct, count });
   } else {
-    const productIndex = cart.findIndex((product) => product.id === productId);
     cart[productIndex].count = cart[productIndex].count + 1;
   }
 
   setStorageItem('cart', cart);
-  displayTotalProductCount(cart, totalProductCountDOM);
+  displayTotalProductCount(cart, totalProductCount);
 };
--- products.js
+++ products.js
@@ -4,7 +4,8 @@ import { getElement, getStorageItem, setStorageItem } from './utils.js';
 import { displayTotalProductCount } from './common.js';
 import { renderAllProducts } from './renderAllProducts.js';
 import { fetchProducts } from './fetchProducts.js';
-import { addSingleProductToCart } from './addProductToCart.js';
+import { addToCart } from './addProductToCart.js';
+import { renderModal } from './renderModal.js';
 
 // API all products
 const url = 'https://fakestoreapi.com/products/';
@@ -25,134 +26,19 @@ const displayFetchedProducts = async () => {
   addToCartBtns.forEach((btn) => {
     btn.addEventListener('click', () => {
       const productId = parseInt(btn.dataset.id);
-      addSingleProductToCart(productId, products, totalProductCount);
+      const selectedProduct = products.find(
+        (product) => product.id === productId
+      );
+
+      addToCart(productId, selectedProduct);
     });
   });
 
   infoBtns.forEach((btn) => {
     btn.addEventListener('click', () => {
       const productId = parseInt(btn.dataset.id);
-      const selectedProduct = products.find(
-        (product) => product.id === productId
-      );
 
-      // SHOW INFO ABOUT PRODUCT
-      const { id, title, image, price, description } = selectedProduct;
-      const modal = document.querySelector('.modal');
-      modal.classList.remove('modal--hide');
-      modal.innerHTML = `<div class="overlay">
-        <article class="single-product">
-          <button class="btn btn--remove single-product__close-btn">
-            <i class="fa-solid fa-xmark"></i>
-          </button>
-          <div class="single-product__inner">
-            <div class="single-product__img-box">
-              <img src="${image}" alt="${image}" class="single-product__img" />
-            </div>
-            <div class="single-product__info">
-              <p class="single-product__price">${price} PLN</p>
-              <h3 class="single-product__title">${title}</h3>
-              <p class="single-product__desc">${description}</p>
-              <div class="single-product__counter-box">
-                <button
-                  class="btn btn--count cart-product__decrease-btn"
-                  data-action="decrease"
-                >
-                  -
-                </button>
-                <p class="cart-product__count">1</p>
-                <button
-                  class="btn btn--count cart-product__increase-btn"
-                  data-action="increase"
-                >
-                  +
-                </button>
-                <button class="btn single-product__add-to-cart-btn" data-action="add" data-id=${id}>
-                  add to cart
-                </button>
-              </div>
-            </div>
-          </div>
-        </article>
-      </div>`;
-
-      // change count
-      const countDOM = modal.querySelector('.cart-product__count');
-      let count = 1;
-      countDOM.textContent = 1;
-
-      const handleBtnClick = (event) => {
-        const btn = event.target;
-
-        if (btn.classList.contains('btn')) {
-          const btnAction = btn.dataset.action;
-
-          if (btnAction === 'increase') {
-            count++;
-            countDOM.textContent = count;
-          }
-
-          if (btnAction === 'decrease') {
-            if (count > 1) {
-              count--;
-            }
-            countDOM.textContent = count;
-          }
-
-          if (btnAction === 'shopping') {
-            modal.classList.add('modal--hide');
-            modal.innerHTML = '';
-            modal.removeEventListener('click', handleBtnClick);
-          }
-
-          if (btnAction === 'add') {
-            let cart = getStorageItem('cart');
-            const productIndex = cart.findIndex(
-              (product) => product.id === productId
-            );
-
-            // check if selected product is in storage
-            const productInCart = cart.find(
-              (product) => product.id === productId
-            );
-            // set product to add
-            if (!productInCart) {
-              cart.push({ ...selectedProduct, count: count });
-              // add cart to storage
-              setStorageItem('cart', cart);
-              // update icon
-              displayTotalProductCount(cart, totalProductCount);
-            } else {
-              cart[productIndex].count =
-                cart[productIndex].count + parseInt(countDOM.textContent);
-              // add cart to storage
-              setStorageItem('cart', cart);
-              // update icon
-              displayTotalProductCount(cart, totalProductCount);
-            }
-
-            // product added message
-            const modalInner = getElement('.single-product__inner');
-            modalInner.classList.add('single-product__inner--added-mes');
-            modalInner.innerHTML = `<p class="single-product__product-added-mes">Product added to your cart! &#x1F389;</p>
-            <div class="single-product__btns-box--added-mes">
-            <button class="btn btn--secondary" data-action="shopping">Continue shopping</button>
-            <button class="btn btn--grey"><a class="single-product__btn-link" href="./cart.html">Go to cart<a></button>
-            </div>
-            `;
-          }
-        }
-      };
-
-      modal.addEventListener('click', handleBtnClick);
-
-      // close modal
-      const closeModalBtn = getElement('.single-product__close-btn');
-      closeModalBtn.addEventListener('click', () => {
-        modal.classList.add('modal--hide');
-        modal.innerHTML = '';
-        modal.removeEventListener('click', handleBtnClick);
-      });
+      renderModal(productId, products);
     });
   });
 };
